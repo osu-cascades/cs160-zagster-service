b2cb3ceb84363862dfd5fe80fff9b0f4d43ad7d1
Yong Joseph Bakos <ybakos@humanoriented.com>
2018-10-29 13:42:30 -0700

    Send resultset rows in response.

--- app.js
+++ app.js
@@ -1,31 +1,26 @@
 require('dotenv').config()
 const express = require('express')
+const { Pool } = require('pg')
 const app = express()
 const PORT = process.env.PORT || 3000
 const DATABASE_URL = process.env.DATABASE_URL || "UNDEFINED"
 
-console.log(DATABASE_URL)
-const { Pool, Client } = require('pg')
-
-const pool = new Pool({
-  connectionString: DATABASE_URL,
+app.get('/', (request, response) => {
+  response.send('I am listening!')
 })
 
-pool.query('SELECT * FROM rides LIMIT 1', (err, res) => {
-  console.log(err, res)
-  pool.end()
+app.get('/zagster', (request, response) => {
+  let data = []
+  const pool = new Pool({
+    connectionString: DATABASE_URL,
+  })
+
+  pool.query('SELECT * FROM rides LIMIT 1', (err, results) => {
+    data = results
+    console.log(err, results)
+    response.send(results.rows[0])
+    pool.end()
+  })
 })
-
-// const client = new Client({
-//   connectionString: DATABASE_URL,
-// })
-// client.connect()
-
-// client.query('SELECT NOW()', (err, res) => {
-//   console.log(err, res)
-//   client.end()
-// })
-
-app.get('/', (req, res) => res.send('I am listening!'))
 
 app.listen(PORT)
