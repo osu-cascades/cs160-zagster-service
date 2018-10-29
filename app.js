6145eba95eb24191205d22a48c8adf07030ad7e6
Yong Joseph Bakos <ybakos@humanoriented.com>
2018-10-29 08:41:53 -0700

    Add pg and simple query to verify connection.

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
+const DATABASE_URL = process.env.DATABASE_URL || "UNDEFINED"
+
+console.log(DATABASE_URL)
+const { Pool, Client } = require('pg')
+
+const pool = new Pool({
+  connectionString: DATABASE_URL,
+})
+
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
+
+// const client = new Client({
+//   connectionString: DATABASE_URL,
+// })
+// client.connect()
+
+// client.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res)
+//   client.end()
+// })
 
 app.get('/', (req, res) => res.send('I am listening!'))
 
 app.listen(PORT)
-
