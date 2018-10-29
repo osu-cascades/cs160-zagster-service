5f2c6e6dee99ff30965e94ac8dd28a9be6b234c0
Yong Joseph Bakos <ybakos@humanoriented.com>
2018-10-29 14:49:04 -0700

    Use port from env.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3001
+const PORT = process.env.PORT || 3001
 
 app.get('/ice_cream', (request,response) => response.send("Mint ice cream"))
 app.get('/RKS', (request, response) => response.send("MintBerryCrunch"))
@@ -28,4 +29,4 @@ app.get('/doodlebob', (request,response) => response.send("Hoy minoy miñoy"))
 app.get('/manbearpig', (request, response) => response.send("Al Gore"))
 app.get('/totallysecurechanneladminonly', (request,response) => response.send("Look at them, they come to this place when they know they are not pure. Tenno use the keys, but they are mere trespassers. Only I, Vor, know the true power of the Void. I was cut in half, destroyed, but through it's Janus Key, the Void called to me. It brought me here and here I was reborn. We cannot blame these creatures, they are being led by a false prophet, an impostor who knows not the secrets of the Void. Behold the Tenno, come to scavenge and desecrate this sacred realm. My brothers, did I not tell of this day? Did I not prophesize this moment? Now, I will stop them. Now I am changed, reborn through the energy of the Janus Key. Forever bound to the Void. Let it be known, if the Tenno want true salvation, they will lay down their arms, and wait for the baptism of my Janus key. It is time. I will teach these trespassers the redemptive power of my Janus key. They will learn it's simple truth. The Tenno are lost, and they will resist. But I, Vor, will cleanse this place of their impurity."))
 
-app.listen(port)
+app.listen(PORT)
