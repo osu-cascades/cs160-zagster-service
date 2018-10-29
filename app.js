ba01e4284d397044b9a4977dd985e56c24a360b5
Yong Joseph Bakos <ybakos@humanoriented.com>
2018-10-29 16:56:16 -0700

    Combine pre-crafted example with student mob implementation.

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 require('dotenv').config()
 const express = require('express')
-const {Pool} = require('pg')
+const { Pool } = require('pg')
 const app = express()
-const PORT = process.env.PORT || 3001
-const DATABASE_URL = process.env.DATABASE_URL || "undefined"
+const PORT = process.env.PORT || 3000
+const DATABASE_URL = process.env.DATABASE_URL || "UNDEFINED"
 
-app.get('/zagster', (request,response) => {
+app.get('/zagster', (request, response) => {
   const pool = new Pool({
-    connectionString: DATABASE_URL
+    connectionString: DATABASE_URL,
   })
 
-  pool.query('SELECT * FROM rides LIMIT 1', (err, results)=> {
+  pool.query('SELECT * FROM rides LIMIT 1', (err, results) => {
     response.send(results.rows[0])
     pool.end()
   })
+})
+
+app.get('/', (request, response) => {
+  response.send('I am listening!')
 })
 
 app.get('/ice_cream', (request,response) => response.send("Mint ice cream"))
@@ -40,6 +44,6 @@ app.get('/gomez', (request, response) => response.send("I am cool"))
 app.get('/white', (request, response) => response.send ("Bulldog Puppies"))
 app.get('/doodlebob', (request,response) => response.send("Hoy minoy miñoy"))
 app.get('/manbearpig', (request, response) => response.send("Al Gore"))
-app.get('/totallysecurechanneladminonly', (request,response) => response.send("Look at them, they come to this place when they know they are not pure. Tenno use the keys, but they are mere trespassers. Only I, Vor, know the true power of the Void. I was cut in half, destroyed, but through it's Janus Key, the Void called to me. It brought me here and here I was reborn. We cannot blame these creatures, they are being led by a false prophet, an impostor who knows not the secrets of the Void. Behold the Tenno, come to scavenge and desecrate this sacred realm. My brothers, did I not tell of this day? Did I not prophesize this moment? Now, I will stop them. Now I am changed, reborn through the energy of the Janus Key. Forever bound to the Void. Let it be known, if the Tenno want true salvation, they will lay down their arms, and wait for the baptism of my Janus key. It is time. I will teach these trespassers the redemptive power of my Janus key. They will learn it's simple truth. The Tenno are lost, and they will resist. But I, Vor, will cleanse this place of their impurity."))
+app.get('/totallysecurechanneladminonly', (request,response) => response.send("Look at them."))
 
 app.listen(PORT)
