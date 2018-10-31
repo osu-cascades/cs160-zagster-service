5bbed45354b3c67f7ac13085c7ef15188547801c
Yong Joseph Bakos <ybakos@humanoriented.com>
2018-10-31 13:01:37 -0700

    Allow CORS.

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const DATABASE_URL = process.env.DATABASE_URL || "UNDEFINED"
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/rides/count', (request, response) => {
   const pool = new Pool({
     connectionString: DATABASE_URL,
