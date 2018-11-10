4e386d277254afcf4b6ac66086f8aa16091ae9da
Yong Joseph Bakos <ybakos@humanoriented.com>
2018-11-09 17:06:41 -0800

    Transformer: Test failure of count_by_year_and_month.

--- transformer.js
+++ transformer.js
@@ -3,6 +3,15 @@
 
 class Transformer {
 
+  constructor() {}
+
+  // {
+  //   2001: [ {1: 1000}, {2: 900}, ... {12: 788} ],
+  //   2002: [ {1: 100}, {2: 90}, ... {12: 78} ]
+  // }
+  static count_by_year_and_month(rows) {
+    return ''
+  }
 
 }
 
