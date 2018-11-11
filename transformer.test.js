430000f23241cb62231bbe57aaf06a528465a50d
Yong Joseph Bakos <ybakos@humanoriented.com>
2018-11-10 23:42:03 -0800

    Align braces.

--- transformer.test.js
+++ transformer.test.js
@@ -8,11 +8,11 @@ test('Transformer exists', () => {
 test('count_by_year_and_month', () => {
   const rows =
     [{"month":1,"year":2001,"count":"20011"},
-      {"month":2,"year":2001,"count":"20012"},
-      {"month":1,"year":2002,"count":"20021"},
-      {"month":2,"year":2002,"count":"20022"},
-      {"month":7,"year":2003,"count":"20037"},
-      {"month":null,"year":null,"count":"100"}]
+     {"month":2,"year":2001,"count":"20012"},
+     {"month":1,"year":2002,"count":"20021"},
+     {"month":2,"year":2002,"count":"20022"},
+     {"month":7,"year":2003,"count":"20037"},
+     {"month":null,"year":null,"count":"100"}]
   const expected_result =
     {2001:[{1:20011},{2:20012}], 2002:[{1:20021},{2:20022}], 2003:[{7:20037}]}
   expect(Transformer.count_by_year_and_month(rows)).toEqual(expected_result)
