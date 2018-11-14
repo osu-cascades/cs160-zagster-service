175155cb717b54d3c733ed52ce48b04b17a2df3b
Yong Joseph Bakos <ybakos@humanoriented.com>
2018-11-13 21:51:49 -0800

    Add semicolons.

--- transformer.js
+++ transformer.js
@@ -10,24 +10,24 @@ class Transformer {
   //   2002: [ {1: 100}, {2: 90}, ... {12: 78} ]
   // }
   static count_by_year_and_month(rows) {
-    let result = {}
+    let result = {};
     rows.map(row => row.year).filter(val => val != null).forEach(year => {
-      if (result[year] === undefined) result[year] = []
-    })
+      if (result[year] === undefined) result[year] = [];
+    });
     rows.forEach(row => {
       if (row.year != null)
-        result[row.year].push({[row.month]:parseInt(row.count)})
-    })
-    return result
+        result[row.year].push({[row.month]:parseInt(row.count)});
+    });
+    return result;
   }
 
   // { 0: 12, 1: 24, ... 23: 788 }
   static count_by_hour(rows) {
-    let result = {}
+    let result = {};
     rows.forEach(row => {
-      if (row.hour != null) result[row.hour] = parseInt(row.count)
-    })
-    return result
+      if (row.hour != null) result[row.hour] = parseInt(row.count);
+    });
+    return result;
   }
 
 }
--- transformer.test.js
+++ transformer.test.js
@@ -12,10 +12,10 @@ test('count_by_year_and_month', () => {
      {'month':1,'year':2002,'count':'20021'},
      {'month':2,'year':2002,'count':'20022'},
      {'month':7,'year':2003,'count':'20037'},
-     {'month':null,'year':null,'count':'100'}]
+     {'month':null,'year':null,'count':'100'}];
   const expected_result =
-    {2001:[{1:20011},{2:20012}], 2002:[{1:20021},{2:20022}], 2003:[{7:20037}]}
-  expect(Transformer.count_by_year_and_month(rows)).toEqual(expected_result)
+    {2001:[{1:20011},{2:20012}], 2002:[{1:20021},{2:20022}], 2003:[{7:20037}]};
+  expect(Transformer.count_by_year_and_month(rows)).toEqual(expected_result);
 });
 
 test('count_by_hour', () => {
@@ -23,7 +23,7 @@ test('count_by_hour', () => {
     [{'hour':0,'count':'429'},
      {'hour':1,'count':'231'},
      {'hour':23,'count':'710'},
-     {'hour':null,'count':'183'}]
-  const expected_result = {'0': 429, '1':231, '23':710}
-  expect(Transformer.count_by_hour(rows)).toEqual(expected_result)
-})
+     {'hour':null,'count':'183'}];
+  const expected_result = {'0': 429, '1':231, '23':710};
+  expect(Transformer.count_by_hour(rows)).toEqual(expected_result);
+});
