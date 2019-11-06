70a39d946bd17cb7587b3fdfd30c43a70f7e577b
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-11-05 22:39:14 -0800

    Examples: Add 2019 student routes.

--- routes/examples.js
+++ routes/examples.js
@@ -2,6 +2,38 @@ const examples = require('express').Router();
 
 examples.get('/', (req, res) => res.send('I am listening!'));
 examples.get('/ice_cream', (req, res) => res.send('Mint ice cream'));
+
+// 2019
+
+examples.get('/you_lazy', (req, res) => res.send('Okay Boomer'));
+examples.get('/cs', (req, res) => res.send('COMPUTER SCIENCE IS AWESOME!'));
+examples.get('/HELLO', (req,res)=> res.send('Welcome!'));
+examples.get('/LightningInABottle', (req,res) => res.send('Blip'));
+examples.get('/help!', (req, res) => res.send('Everything is going to be ok.'));
+examples.get('/why_did_the_chicken_cross_the_road',(req, res) => res.send('Well?'));
+examples.get('/howdy', (req, res) => res.send('Howdy, there partner!!'));
+examples.get('/how_are_you_sugar', (req, res) => res.send('Better now you\'re here!'));
+examples.get('/winter', (req, res) => res.send('S N O W!'));
+examples.get('/bend',(req,res)=> res.send('Beer and Puffys'));
+examples.get('/yeet', (req, res) => res.send('OOF'));
+examples.get('/hi', (req, res) => res.send('hello back'));
+examples.get('/num nums', (req, res) => res.send('food'));
+examples.get('/pizza', (req, res) => res.send('Pepperoni Pizza'));
+examples.get('/uh_oh', (req, res) => res.send('Stinky'));
+examples.get('/tired', (req,res) => res.send ('Ligma'));
+examples.get('/why_frog_sad', (req, res) => res.send('His car got toad :('));
+examples.get('/loss', (req, res) => res.send('|, ||,||, |_'));
+examples.get('/listening', (req, res) => res.send('Nobody is listening!'));
+examples.get('/best_dog', (req, res) => res.send('All doggos are the best!'));
+examples.get('/weather', (req, res) => res.send('Look out the window!'));
+examples.get('/dogs', (req, res) => res.send('Adopt don\'t shop!'));
+examples.get('/money', (req, res) => res.send('Show me the money'));
+examples.get('/minecraft', (req, res) => res.send('*Digs straight down*'));
+examples.get('/query', (req, res) => res.send('idk google it'));
+examples.get('/please_help', (req,res) => res.send('Why do I care?'));
+
+// 2018
+
 examples.get('/RKS', (req, res) => res.send('MintBerryCrunch'));
 examples.get('/HemenwayThanksgiving', (req, res) => res.send('SweetPotato'));
 examples.get('/Cat_nya', (req, res) => res.send('Nyaaaaaaaaaaaaaaa!'));
@@ -28,3 +60,7 @@ examples.get('/manbearpig', (req, res) => res.send('Al Gore'));
 examples.get('/totallysecurechanneladminonly', (req, res) => res.send('Look at them.'));
 
 module.exports = examples;
+
+
+
+
